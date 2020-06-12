cb57d84019a9155de2086c43b66df83df151bfb0
Seshagiri H Prasad <Seshagiri.Prasad@in.ey.com>
2020-06-12 09:56:22 +0530

    updated our menu-item to navigation withReacter HOC

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
 import "./App.css";
 
 import HomePage from "./pages/homepage/homepage.component";
 
+const HatPage = () => (
+  <div>
+    <h1>HatS PAGE</h1>
+  </div>
+);
+
 function App() {
   return (
-    <div>
-      <HomePage />
-    </div>
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/hats" component={HatPage} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
